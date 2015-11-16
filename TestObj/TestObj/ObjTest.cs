6d4abdc57f83062b82690fb06a0a81cfe7487e61
Masoor ahmed <masroor.ahmed@gaditek.com>
2015-11-16 17:25:30 +0500

    initial commit

---
+++ TestObj/TestObj/ObjTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjTest
+{
+    [TestFixture]
+    public class ObjTest
+    {
+        int i = 0;
+
+        [SetUp]
+        protected void SetUp()
+        {
+            i = 4;
+        }
+
+        [Test]
+        public void TwoPlusTwoFour()
+        {
+            Assert.AreEqual(2 + 2, i);
+        }
+    }
+}
